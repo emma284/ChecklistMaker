22f44130b954dc289709730ba1b0088708430d4f
Emmanuel <emma_a284@hotmail.com>
2019-09-12 11:35:17 -0300

    Eliminadas las lineas autogeneradas de seccionController

--- src/AppBundle/Controller/SeccionController.php
+++ src/AppBundle/Controller/SeccionController.php
@@ -42,55 +42,5 @@ class SeccionController extends Controller{
 
         // actually executes the queries (i.e. the INSERT query)
         $entityManager->flush();
-
-<<<<<<< refs/remotes/origin/devEmmanuel
-
-    /**
-     * @Route("/seccion/mostrar/{id}/")
-     */
-   /* public function showAction($id)
-    {
-        $seccion = $this->getDoctrine()->getRepository(Seccion::class)->find($id);
-        
-        if (!$seccion) {
-            throw $this->createNotFoundException('No se encontró seccion que tiene este id '.$id);
-        }
-        else{
-            return new Response('Se encontró el seccion con id: '.$seccion->getId().'. Está ubicado en calle: '.$seccion->getCalle().' al '.$seccion->getNumero());
-        }
-    }*/
-
-    /**
-     * @Route("/seccion/form/", name="laRutaVieja")
-     */
-    //Formularios con seteo de datos por defecto
-   /* public function new2(Request $request)
-    {
-
-        // creates a seccion and gives it some dummy data for this example
-        $seccion = new Seccion();
-        $form = $this->createForm(SeccionType::class, $seccion);
-
-        // catch all data
-        $form->handleRequest($request);
-
-        
-        // validate all data and if it success save them
-        if ($form->isSubmitted() && $form->isValid()) {
-            
-            $task = $form->getData();
-
-
-            return $this->redirectToRoute('laRuta');
-        }
-        
-        return $this->render('seccion/new.html.twig', [
-            'form' => $form->createView(),
-        ]);
-
-=======
-        return new Response('Saved new product with id '.$seccion->getId());
->>>>>>> agregado dc2type
     }
-
 }
